acc688b9a7c494e0c3a8daf3698c422e9cb42413
AlexhpSENATI <1510696@senati.pe>
2024-09-11 00:26:08 -0500

    corregido front

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-# # app.py
-# from flask import Flask, render_template
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-# # Ruta para la página de login
-# @app.route('/')
-# def login():
-#     return render_template('login.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 from flask import Flask, render_template
 
 app = Flask(__name__)
